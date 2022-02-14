dfa5899d7a2d9008854e5e8c3306455cca163113
Patial <npatial@ford.com>
2022-02-14 11:42:17 +0530

    Added Welcome message

--- src/com/ford/Runner.java
+++ src/com/ford/Runner.java
@@ -4,6 +4,7 @@ import com.ford.controller.PEMService;
 
 public class Runner {
     public static void main(String[] args) {
+        System.out.println("Welcome to My app : ");
         PEMService service  = new PEMService();
         service.showMenu();
     }
